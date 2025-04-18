92a4ce9ba5ea17828f09329a1640ec759597aa64
Alexander Chudin <chudinsasha1@gmail.com>
2025-04-19 02:42:58 +0300

    Add more animations and add script for base moving

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 movement;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        Flip();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Flip()
+    {
+        if (movement.x > 0.01f)
+            spriteRenderer.flipX = false;
+        else if (movement.x < -0.01f)
+            spriteRenderer.flipX = true;
+    }
+}
+
